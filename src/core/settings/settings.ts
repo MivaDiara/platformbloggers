8373492a190c707cd5b67fc4dc9be386139e98b7
EllyVeny <nastyrakova04@gmail.com>
2026-06-28 18:18:38 +0700

    settings fix

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -1,6 +1,6 @@
 export const SETTINGS = {
     PORT: process.env.PORT || 5003,
     MONGO_URL:
-        process.env.MONGO_URL || 'mongodb://localhost:27017/',
+        process.env.MONGO_URL || 'mongodb+srv://nastyrakova04_db_user:Yfcnz1003042004.@cluster0.lpofgf9.mongodb.net/?appName=Cluster0',
     DB_NAME: process.env.DB_NAME || 'bloggersplatform',
 };
